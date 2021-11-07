a99517e6ccb666e69bb7a0c226e2c58f05e9eb9f
SaulColli <saulmisaelcolli@gmail.com>
2021-11-07 15:50:21 -0500

    Se ha agregado el proyecto

---
+++ com/tuempresa/faturaciongit/modelo/Cliente.java
@@ -0,0 +1,20 @@
+package com.tuempresa.faturaciongit.modelo;
+
+import javax.persistence.*;
+
+import org.openxava.annotations.*;
+
+import lombok.*;
+
+@View(name="Simple", members="numero, nombre")
+@Entity @Getter @Setter
+public class Cliente {
+@Id
+@Column(length=6)
+int numero;
+
+@Required
+@Column(length=50)
+String nombre;
+
+}
