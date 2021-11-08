af441d5ef975be631674f6ff6bc98ebd7b41e555
erikahernande <erika.hernandezmay@cbtis72.edu.mx>
2021-11-08 07:30:04 -0500

    rama_erika añadida

---
+++ com/tuempresa/faturaciongit/modelo/Producto.java
@@ -0,0 +1,41 @@
+package com.tuempresa.facturaciongit.modelo;
+import java.math.*;
+
+import javax.persistence.*;
+
+import org.openxava.annotations.*;
+
+import lombok.*;
+ 
+@Entity @Getter @Setter
+public class Producto {
+	@Id 
+	@Column(length=9)
+    int numero;
+ 
+    
+	@Column(length=50) 
+	@Required
+    String descripcion;
+    
+    
+    @ManyToOne( 
+    fetch=FetchType.LAZY, 
+    optional=true) 
+    @DescriptionsList 
+    Categoria categoria; 
+    
+    @ManyToOne(fetch=FetchType.LAZY)
+    @DescriptionsList
+    Autor autor;
+    
+    @Stereotype("DINERO")
+    BigDecimal precio;
+     
+    @Stereotype("GALERIA_IMAGENES")
+    @Column(length=32)
+    String fotos;
+     
+    @Stereotype("TEXTO_GRANDE")
+    String observaciones;
+}
