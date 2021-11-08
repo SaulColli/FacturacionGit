7817f3d32b0761c7e996c071163d3bb07848b562
SaulColli <93833625+SaulColli@users.noreply.github.com>
2021-11-07 21:29:57 -0500

    Add files via upload

---
+++ com/tuempresa/faturaciongit/modelo/Identificable.java
@@ -0,0 +1,18 @@
+package com.tuempresa.faturaciongit.modelo;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.*;
+import org.openxava.annotations.*;
+
+import lombok.*;
+
+@MappedSuperclass @Getter @Setter
+public class Identificable {
+
+	@Id @Hidden
+	@GeneratedValue(generator="system-uuid")
+	@GenericGenerator(name="system-uuid", strategy="uuid")
+	@Column(length=32)
+	String oid;
+}
